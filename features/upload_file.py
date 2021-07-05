3b9676872e0fc2b48369258d250e564b8121444e
fernando cruz <frlc_31@hotmail.com>
2021-07-05 16:41:16 -0300

    Update upload_file.py

--- features/upload_file.py
+++ features/upload_file.py
@@ -3,7 +3,7 @@ import dropbox
 
 file_name='deploy.zip'
 dropbox_path='/var/jenkins_home/workspace/pipeline@2/features/'
-dbx=dropbox.Dropbox('KY62MTunDIAAAAAAAAAADSjVQZpmPbvnTs-E30swYKUPvB97rAW41FZ1qTV4YZZ4')
+dbx=dropbox.Dropbox('')
 
 
 with open(file_name, 'rb') as f:
